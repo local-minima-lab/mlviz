c61a4dc84c546b372c77c6d5f97ffb9319244f66
zaidansani <mzaidanbs@gmail.com>
2025-12-27 10:15:24 +0800

    Add test deployment for backend on GCP

--- backend/app.py
+++ backend/app.py
@@ -25,10 +25,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-    ],
+    allow_origins=settings.get_all_origins(),
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["Content-Type", "Authorization"],
