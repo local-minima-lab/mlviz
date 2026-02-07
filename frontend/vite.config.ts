7c572664008f3b993222081e193a63a212706a5e
zaidansani <mzaidanbs@gmail.com>
2026-02-08 02:12:06 +0800

    Change vite base

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,13 +14,15 @@ export default defineConfig(({ mode }) => ({
     },
     server: {
         // Only use proxy in development mode
-        proxy: mode === "development" ? {
-            // Proxying requests that start with /api
-            "/api": {
-                target: "http://127.0.0.1:8000", // Your Flask backend address
-                changeOrigin: true, // Needed for virtual hosted sites
-                // rewrite: (path) => path.replace(/^\/api/, ""), // Only if your backend routes were e.g., /dt/train_model and not /api/dt/train_model
-            },
-        } : undefined,
+        proxy:
+            mode === "development"
+                ? {
+                      // Proxying requests that start with /api
+                      "/api": {
+                          target: "http://127.0.0.1:8000",
+                          changeOrigin: true,
+                      },
+                  }
+                : undefined,
     },
 }));
